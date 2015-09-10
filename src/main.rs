e5827bc412f9f50406fe25e4065504eb37e9e880
Helge Skogly Holm <helge.holm@gmail.com>
2015-09-10 17:22:54 +0200

    Prettier exampe output dice.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn roll<T: Rng>(rng: &mut T) -> String {
 fn main() {
     let seed = rand::thread_rng().next_u32();
     let mut rng = prng_well1024a::Well1024aRng::new(seed);
-    println!("{}, {}, {}, {}, {}",
+    println!("{} {} {} {} {}",
              roll(&mut rng),
              roll(&mut rng),
              roll(&mut rng),
