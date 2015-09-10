70dadaf160067c4753455f1daf199d1aa9403cc6
Helge Skogly Holm <helge.holm@gmail.com>
2015-09-10 10:39:48 +0200

    Made binary test more interesting.

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,19 @@ extern crate rand;
 
 use rand::Rng;
 
+fn roll<T: Rng>(rng: &mut T) -> String {
+    let options = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+    let result = rng.choose(&options).expect("die fell on floor");
+    String::from(*result)
+}
+
 #[allow(dead_code)]
 fn main() {
-    fn inner<T: Rng>(mut rng: T) {
-        let mut i = 1_000;
-        loop {
-            let options = ["apple", "tails"];
-            let result = rng.choose(&options)
-                .expect("choice failed");
-            println!("{}", result);
-            //rng.next_u32();
-            i -= 1;
-            if i == 0 {
-                break;
-            }
-        }
-    }
-    inner(prng_well1024a::Well1024aRng::new());
+    let mut rng = prng_well1024a::Well1024aRng::new();
+    println!("{}, {}, {}, {}, {}",
+             roll(&mut rng),
+             roll(&mut rng),
+             roll(&mut rng),
+             roll(&mut rng),
+             roll(&mut rng) );
 }
